89da46b3b8e61e0ba349802b73b8f036dda703a1
Jaxee_ <50408857+studio-mad-tagic@users.noreply.github.com>
2021-11-08 18:16:17 +0300

    .

--- discord_bot/d_commands/info.py
+++ discord_bot/d_commands/info.py
@@ -4,6 +4,7 @@ async def info_cmd(bot, discord, message, botconfig, platform, os, datetime, one
   info_content.add_field(name=str(localization[1][15][2]), value=str("Python with discord.py library"), inline=True)
   info_content.add_field(name=str(localization[1][15][3]), value="Jaxee (`" + bot.get_user(int(botconfig['owner'])).name + "#" + str(bot.get_user(int(botconfig['owner'])).discriminator) + "`)", inline=True)
   if bot.user.id == 901352871071715408:
-  	info_content.add_field(name=str(localization[1][15][4]), value=str(localization[1][15][5]), inline=True)
+    info_content.add_field(name=str(localization[1][15][4]), value=str(localization[1][15][5]), inline=True)
+    info_content.add_field(name=str(localization[1][15][6]), value=str(localization[1][15][7]), inline=True)
   info_content.set_footer(text="© Jaxee, 2021-2021")
   await message.channel.send(embed=info_content)
